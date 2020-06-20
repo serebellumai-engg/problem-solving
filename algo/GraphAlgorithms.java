b37d408328e1504a557bf9757003aae93bad32cc
Avinash Chandwani <avinashchandwani@elements-mac0497.local>
2020-06-20 16:07:45 +0530

    Dijkstra Algorithm

--- algo/GraphAlgorithms.java
+++ algo/GraphAlgorithms.java
@@ -9,7 +9,9 @@ public class GraphAlgorithms {
 
     public static void main(String []args){
         GraphAlgorithms algos = new GraphAlgorithms();
-        algos.printShortestPathFromSourceUndirectedGraph();
+        //algos.printShortestPathFromSourceUndirectedGraph();
+        DijkstraShortestPath shortestPath = new DijkstraShortestPath(5);
+        shortestPath.dijkstra(0);
     }
 
     List<Integer> getTopologicalSortDFS(GraphAdj adj){
@@ -156,5 +158,93 @@ public class GraphAlgorithms {
             System.out.println(path.get(k) + " ");
         }
     }
+}
+
+class DijkstraShortestPath {
+
+    private int V;
+    private Set<Integer> settled;
+    private PriorityQueue<WeightedNode> pq;
+    private int []dist;
+    private List<List<WeightedNode>> adj;
+
+    public DijkstraShortestPath(int V){
+        this.V = V;
+        dist = new int[V];
+        pq = new PriorityQueue<WeightedNode>(V, new WeightedNode());
+        settled = new HashSet<>();
+        init();
+    }
+
+    private void init(){
+        adj = new ArrayList<List<WeightedNode>>(); 
+        for (int i = 0; i < V; i++) { 
+            List<WeightedNode> item = new ArrayList<WeightedNode>(); 
+            adj.add(item); 
+        } 
+  
+        // Inputs for the DPQ graph 
+        adj.get(0).add(new WeightedNode(1, 9)); 
+        adj.get(0).add(new WeightedNode(2, 6)); 
+        adj.get(0).add(new WeightedNode(3, 5)); 
+        adj.get(0).add(new WeightedNode(4, 3)); 
+        adj.get(2).add(new WeightedNode(1, 2)); 
+        adj.get(2).add(new WeightedNode(3, 4)); 
+    }
+
+    public void dijkstra(int src){
+        for(int i=0; i<V; i++)
+            dist[i] = Integer.MAX_VALUE;
+        pq.add(new WeightedNode(src, 0));
+        dist[src] = 0;
+        while(settled.size() != V){
+            int u = pq.remove().node;
+            settled.add(u);
+            processNeighbours(u);
+        }
+
+        System.out.println("The shorted path from node :"); 
+        for (int i = 0; i < dist.length; i++) 
+            System.out.println(src + " to " + i + " is "
+                               + dist[i]); 
+    }
+
+    private void processNeighbours(int u){
+        int edgeDistance = -1, newDistance = -1;
+        for(int i=0;i< adj.get(u).size();i++){
+            WeightedNode v = adj.get(u).get(i);
+            if(!settled.contains(v.node)){
+                edgeDistance = v.cost;
+                newDistance = dist[u] + edgeDistance;
+                if(newDistance < dist[v.node]){
+                    dist[v.node] = newDistance;
+                }
+                pq.add(new WeightedNode(v.node, dist[v.node]));
+            }
+        }
+    }
+}
+
+class WeightedNode implements Comparator<WeightedNode>{
+    int node;
+    int cost;
+
+    public WeightedNode(){
+
+    }
+
+    public WeightedNode(int node, int cost){
+        this.node = node;
+        this.cost = cost;
+    }
+
+    public int compare(WeightedNode node1, WeightedNode node2){
+        if(node1.cost < node2.cost)
+            return -1;
+        if(node1.cost > node2.cost)
+            return 1;
+        return 0;
+    }
+
 }
 
