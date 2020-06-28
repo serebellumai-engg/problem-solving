ec130fc68f231f4fea5b15ad8a3bd86c751c03e9
Avinash Chandwani <avinashchandwani@elements-mac0497.local>
2020-06-28 17:03:55 +0530

    DP:Min Coin Change

---
+++ algo/DynamicProgramming.java
@@ -0,0 +1,38 @@
+package algo;
+public class DynamicProgramming{
+
+    public static void main(String []args){
+        int []coins = {1,2,3};
+        MinimumCoinChange coinChange = new MinimumCoinChange(coins, 5);
+        System.out.println(coinChange.minCoinChange());
+    }
+}
+
+class MinimumCoinChange{
+
+    int change;
+    int []coins;
+    public MinimumCoinChange(int []coins, int change){
+        this.change = change;
+        this.coins = coins;
+    }
+    
+    public int minCoinChange(){
+        int table[] = new int[change + 1];
+        for(int i = 0; i<table.length;i++){
+            table[i] = Integer.MAX_VALUE;
+        }
+        table[0] = 0;
+        for(int i = 1; i<= change; i++){
+            for(int j =0 ; j < coins.length; j++){
+                if(coins[j] <= i){
+                    int subResult = table[i-coins[j]];
+                    if(subResult != Integer.MAX_VALUE && subResult + 1 < table[i]){
+                        table[i] = subResult + 1;
+                    }
+                }
+            }
+        }
+        return table[change];
+    }
+}
